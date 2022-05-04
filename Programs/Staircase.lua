b0670fdf5384c911e502917f197165b8df60f9aa
Drew Gilliland <dkg36959@uga.edu>
2022-05-04 16:47:15 -0400

    Added staricase function

---
+++ Programs/Staircase.lua
@@ -0,0 +1,27 @@
+function StairCase(y)
+    local count = 0
+
+    while (count ~= y) do
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        turtle.dig()
+        turtle.forward()
+        turtle.digUp()
+        turtle.back()
+        turtle.digDown()
+        turtle.down()
+
+        count = count + 1
+    end
+end
+
+function Main()
+    term.clear()
+    term.setCursorPos(1, 1)
+    io.write("Enter desired Y level: ")
+    local y = io.read()
+    StairCase(y)
+end
+
+Main()
