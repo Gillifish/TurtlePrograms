c6f57436dc034b48723deb5a825ab3443acf1e0b
Drew Gilliland <dkg36959@uga.edu>
2022-05-04 16:59:27 -0400

    Staircase Changes

--- Programs/Staircase.lua
+++ Programs/Staircase.lua
@@ -1,6 +1,6 @@
-function StairCase(y)
+function Staircase(y)
     local count = 0
-
+ 
     while (count ~= y) do
         turtle.dig()
         turtle.forward()
@@ -11,17 +11,17 @@ function StairCase(y)
         turtle.back()
         turtle.digDown()
         turtle.down()
-
+ 
         count = count + 1
     end
 end
-
+ 
 function Main()
     term.clear()
     term.setCursorPos(1, 1)
     io.write("Enter desired Y level: ")
     local y = io.read()
-    StairCase(y)
+    Staircase(y)
 end
-
+ 
 Main()
